6c6bc0416d4d52d728fed6740814a21423de142c
kaekacha <56618353+kaekacha@users.noreply.github.com>
2021-04-12 13:36:42 -0400

    updated files

--- app.js
+++
@@ -1,5 +0,0 @@
-//read the json data file
-d3.json("././samples.json", function(x) {
-    console.log(x)
-});
-
---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+d3.json("samples.json", function(x) {
+    console.log(x);
+});
+
+// let metadata = samples.metadata;
+// console.log(metadata);
