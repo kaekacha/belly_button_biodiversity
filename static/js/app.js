5f238a1661b014c2b928e39438268e95853018ad
kaekacha <56618353+kaekacha@users.noreply.github.com>
2021-04-12 14:30:59 -0400

    continued refining app.js code

--- static/js/app.js
+++ static/js/app.js
@@ -1,66 +1,32 @@
-  
-// select the user input field
-var idSelect = d3.select("#selDataset");
+  // create varaibles for each of the html id tags
+var id = d3.select("#selDataset");
+var table = d3.select("#sample-metadata");
+var bar = d3.select("#bar");
+var bubble = d3.select("bubble");
+var gauge = d3.select("gauge");
 
-// select the demographic info div's ul list group
-var demographicsTable = d3.select("#sample-metadata");
-
-// select the bar chart div
-var barChart = d3.select("#bar");
-
-// select the bubble chart div
-var bubbleChart = d3.select("bubble");
-
-// select the gauge chart div
-var gaugeChart = d3.select("gauge");
-
-// create a function to initially populate dropdown menu with IDs and draw charts by default (using the first ID)
+// this function populates dropdown menu with IDs and draw charts by default (using the first ID)
 function init() {
+    resetData(); // reset any previous data; see function definition below
 
-    // reset any previous data
-    resetData();
-
-    // read in samples from JSON file
     d3.json("data/samples.json").then((data => {
-
-        // ----------------------------------
-        // POPULATE DROPDOWN MENU WITH IDs 
-        // ----------------------------------
-
-        //  use a forEach to loop over each name in the array data.names to populate dropdowns with IDs
         data.names.forEach((name => {
-            var option = idSelect.append("option");
-            option.text(name);
-        })); // close forEach
+            id.append("option").text(name);
+        }));
+        plotCharts(id.property("value")); // plot charts with value ID changed in drop down menu; see function definition below
+    })); 
+}
 
-        // get the first ID from the list for initial charts as a default
-        var initId = idSelect.property("value")
-
-        // plot charts with initial ID
-        plotCharts(initId);
-
-    })); // close .then()
-
-} // close init() function
-
-// create a function to reset divs to prepare for new data
+// this function resets the html id tags
 function resetData() {
-
-    // ----------------------------------
-    // CLEAR THE DATA
-    // ----------------------------------
-
-    demographicsTable.html("");
-    barChart.html("");
-    bubbleChart.html("");
-    gaugeChart.html("");
-
-}; // close resetData()
+    table.html("");
+    bar.html("");
+    bubble.html("");
+    gauge.html("");
+}; 
 
 // create a function to read JSON and plot charts
 function plotCharts(id) {
-
-    // read in the JSON data
     d3.json("data/samples.json").then((data => {
 
         // ----------------------------------
@@ -76,7 +42,7 @@ function plotCharts(id) {
         // Iterate through each key and value in the metadata
         Object.entries(individualMetadata).forEach(([key, value]) => {
 
-            var newList = demographicsTable.append("ul");
+            var newList = table.append("ul");
             newList.attr("class", "list-group list-group-flush");
 
             // append a li item to the unordered list tag
